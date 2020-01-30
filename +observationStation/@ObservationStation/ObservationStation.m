d8569559abb8db1844a566caae76b80c56454e5b
Robin Shaun <robin_shaun@foxmail.com>
2020-01-30 21:19:21 +0800

    修改测站类，使其输入输出维数合理，并修改readme

--- +observationStation/@ObservationStation/ObservationStation.m
+++ +observationStation/@ObservationStation/ObservationStation.m
@@ -4,38 +4,50 @@ classdef ObservationStation
     %   Detailed explanation goes here
     
     properties
-        Glatitude %地理纬度，rad
-        s %当地真恒星时，秒
-        altitude %当地海拔，m
-        azimuth %方位角，rad
-        elevation %仰角（高低角），rad
-        distance %航天器与测站的距离，m
-        Dazimuth %方位角变化率，rad/s
-        Delevation %仰角变化率，rad/s
-        Ddistance %航天器与测站的距离变化率，m/s
-        stationPos %地心惯性系下测站位置，m
-        C %测站坐标系到地心惯性系的方向余弦阵
-        spacecraftPos %地心惯性系下航天器位置，m
-        spacecraftVel %地心惯性系下航天器速度，m/s
+        Glatitude %地理纬度，标量，rad
+        s %当地真恒星时，数组，秒
+        altitude %当地海拔，标量，m
+        azimuth %方位角，数组，rad
+        elevation %仰角（高低角），数组，rad
+        distance %航天器与测站的距离，数组，m
+        Dazimuth %方位角变化率，数组，rad/s
+        Delevation %仰角变化率，数组，rad/s
+        Ddistance %航天器与测站的距离变化率，数组，m/s
+        stationPos %测站位置（在地心惯性系下），数组，m
+        spacecraftPos %航天器位置（在地心惯性系下），数组，m
+        spacecraftVel %航天器速度（在地心惯性系下），数组，m/s
     end
     
     methods
         function obj = ObservationStation(B,s,H,A,E,D,Adot, Edot,Ddot)
             %构造函数
             %把角度都转为弧度
-            obj.Glatitude = B/180*pi;
+            %这样的处理是为了解决输入和输出的维数的合理性
+            for i=1:length(A)
+                obj.Glatitude = B/180*pi;
+                obj.s = s(i)/180*pi; 
+                obj.altitude = H;
+                obj.azimuth = A(i)/180*pi; 
+                obj.elevation = E(i)/180*pi; 
+                obj.distance = D(i);
+                obj.Dazimuth = Adot(i);
+                obj.Delevation = Edot(i);
+                obj.Ddistance = Ddot(i);
+                obj.stationPos(:,i) = obj.stationpos();
+                C = obj.station2inertial(obj.s,obj.Glatitude);
+                obj.spacecraftPos(:,i) = obj.spacecraftpos(C,i);
+                obj.spacecraftVel(:,i) = obj.spacecraftvel(C,i);   
+            end         
             obj.s = s/180*pi; 
-            obj.altitude = H;
             obj.azimuth = A/180*pi; 
             obj.elevation = E/180*pi; 
             obj.distance = D;
             obj.Dazimuth = Adot;
             obj.Delevation = Edot;
             obj.Ddistance = Ddot;
-            obj.stationPos = obj.stationpos();
-            obj.C = obj.station2inertial(obj.s,obj.Glatitude);
-            obj.spacecraftPos = obj.spacecraftpos();
-            obj.spacecraftVel = obj.spacecraftvel();            
+            obj.stationPos = obj.stationPos';
+            obj.spacecraftPos = obj.spacecraftPos';
+            obj.spacecraftVel = obj.spacecraftVel';
         end
         
         function stationpos = stationpos(obj)
@@ -50,14 +62,14 @@ classdef ObservationStation
             C=[-sin(s_),-cos(s_)*sin(B_),cos(s_)*cos(B_);cos(s_),-sin(s_)*sin(B_),sin(s_)*cos(B_);0,cos(B_),sin(B_)];
         end
         
-        function spacecraftpos = spacecraftpos(obj) 
+        function spacecraftpos = spacecraftpos(obj,C,i) 
             rou = obj.distance*[cos(obj.elevation)*sin(obj.azimuth),cos(obj.elevation)*cos(obj.azimuth),sin(obj.elevation)]';
-            spacecraftpos = obj.stationPos + obj.C*rou;
+            spacecraftpos = obj.stationPos(:,i) + C*rou;
         end
 
-        function spacecraftvel = spacecraftvel(obj)
+        function spacecraftvel = spacecraftvel(obj,C,i)
             import constants.AstroConstants
-            spacecraftvel = obj.C*[obj.Ddistance*cos(obj.elevation)*sin(obj.azimuth)-obj.distance*obj.Delevation*sin(obj.elevation)*sin(obj.azimuth)+obj.distance*obj.Dazimuth*cos(obj.elevation)*cos(obj.azimuth),obj.Ddistance*cos(obj.elevation)*cos(obj.azimuth)-obj.distance*obj.Delevation*sin(obj.elevation)*cos(obj.azimuth)-obj.distance*obj.Dazimuth*cos(obj.elevation)*sin(obj.azimuth),obj.Ddistance*sin(obj.elevation)+obj.distance*obj.Delevation*cos(obj.elevation)]'+cross([0,0,AstroConstants.we]',obj.spacecraftPos);
+            spacecraftvel = C*[obj.Ddistance*cos(obj.elevation)*sin(obj.azimuth)-obj.distance*obj.Delevation*sin(obj.elevation)*sin(obj.azimuth)+obj.distance*obj.Dazimuth*cos(obj.elevation)*cos(obj.azimuth),obj.Ddistance*cos(obj.elevation)*cos(obj.azimuth)-obj.distance*obj.Delevation*sin(obj.elevation)*cos(obj.azimuth)-obj.distance*obj.Dazimuth*cos(obj.elevation)*sin(obj.azimuth),obj.Ddistance*sin(obj.elevation)+obj.distance*obj.Delevation*cos(obj.elevation)]'+cross([0,0,AstroConstants.we]',obj.spacecraftPos(:,i));
         end
     end
 end
--- readme.md
+++ readme.md
@@ -27,3 +27,35 @@ timesystem =
       smean: 6.2995e+08
           s: 6.2995e+08
 ```
+
+## 3.测站
+根据
+- 测站纬度、真太阳时、海拔（或高程）
+- 航天器方位角、高低角、距离、方位角变化率、高低角变化率和距离变化率
+
+算出
+-  测站位置（在地心惯性系下）
+- 航天器位置（在地心惯性系下）
+- 航天器速度（在地心惯性系下）
+
+注意，纬度和海拔数据必须是标量，其余可以是数组
+```
+observationstation = observationStation.ObservationStation(latitude,timesystem.s/3600*15,altitude,azimuth,elevation,distance,Dazimuth,Delevation,Ddistance)
+
+observationstation = 
+
+  ObservationStation with properties:
+
+        Glatitude: 0.1024
+                s: [30×1 double]
+         altitude: 1000
+          azimuth: [30×1 double]
+        elevation: [30×1 double]
+         distance: [30×1 double]
+         Dazimuth: [30×1 double]
+       Delevation: [30×1 double]
+        Ddistance: [30×1 double]
+       stationPos: [30×3 double]
+    spacecraftPos: [30×3 double]
+    spacecraftVel: [30×3 double]
+```
